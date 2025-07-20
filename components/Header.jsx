b932fb9e344904d0c427bf433c1eb68886cb5cc6
Akane-Asahi <111799231+Akane-Asahi@users.noreply.github.com>
2025-07-20 07:22:52 +0600

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Qur'an Semantic Search</h1>
+      <p>Ask a question to find matching verses</p>
+    </header>
+  )
+}
+
+export default Header
